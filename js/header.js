32af144e09ae4023e23a61f25d6476975d89ba6a
Spimods <132432388+Spimods@users.noreply.github.com>
2024-04-27 20:48:46 +0200

    maj

--- js/header.js
+++ js/header.js
@@ -1,10 +1,4 @@
-const headerContainer = document.querySelector('.header-container');
-function adjustHeaderHeight() {
-  const windowHeight = window.innerHeight;
-  const headerHeight = headerContainer.offsetHeight;
-  const extraHeight = 0.09 * windowHeight; // Utiliser 10% de la hauteur de la fenêtre pour l'ajustement
-  const newHeight = `calc(${headerHeight}px + ${extraHeight}px)`;
-  headerContainer.style.height = newHeight;
-}
-window.addEventListener('load', adjustHeaderHeight);
-window.addEventListener('resize', adjustHeaderHeight);
+$(".btn2").on("click", function(){
+    $(".btn2").toggleClass("close-btn");
+    $(".menu").toggleClass("open-menu");
+  });
